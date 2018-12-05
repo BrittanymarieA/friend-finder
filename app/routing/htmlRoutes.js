c851ea671a3ae4a227b2858cbcb2ef44e9801b00
brittanymariea <brittanymariealdaco@gmail.com>
2018-12-04 21:04:33 -0700

    server update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = function (app){
 //creating route to home page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "./../public/home.html"));
+    res.sendFile(path.join(__dirname + "./../public/index.html"));
 });
 
 //creating route to survey page
