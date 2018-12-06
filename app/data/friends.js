2fd7ea033de56e48394155f14e53f0037adb29d3
brittanymariea <brittanymariealdaco@gmail.com>
2018-12-05 18:15:58 -0700

    fixing bugs, edditing friends pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,102 @@
 
- let friends = [
+var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      },
-]
+      name: "Spongebob",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNA8oartk82pEMt0rAqmoM2Ng5oY66fLdj3xwRKLnYT5huLSBk_A",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jorege",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVR5-ISkyjpmWCFGrzQUrC6zkyOuangTdb60Tlx-K2Yqh8xuCHw",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Jacob",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS65DxkH6PgpCbPkz8rcMghigA2QmBPFrY-MeuHmJ06tNEW8KCN",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Luke",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6i75RHvpJJRJkD5lJPnsXOdclHFeByYU6QwZmQCnb1La84mPh",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Ralph",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsA-0OPJCZ-3WhE7fOFQHmkcAj_D3aBxyYfY3XRfpG5thGfi2m",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Bison",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS93eSA7VEL8Qp5N44C320E8Bd0-9tr2ytVrZgvJ9Q4DluOYz0W",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
 
 module.exports = friends;
